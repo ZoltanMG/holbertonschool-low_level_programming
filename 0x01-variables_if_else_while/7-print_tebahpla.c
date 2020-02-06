dcdc724a93e3b96687ce3ec513d6362c6efde318
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-06 16:15:46 -0500

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letra = 'z';
+while (letra >= 'a')
+{
+putchar(letra);
+letra--;
+}
+putchar('\n');
+return (0);
+}
