a89b8a59eb2e56448274cd3b67da232a549ca5be
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-06 16:20:13 -0500

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int m = '0';
+int M = 'a';
+while (m <= '9')
+{
+putchar(m);
+m++;
+}
+while (M <= 'f')
+{
+putchar(M);
+M++;
+}
+putchar('\n');
+return (0);
+}
