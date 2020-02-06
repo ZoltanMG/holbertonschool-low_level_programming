ffa07e998ebf46ca375373c60f8b3418b636cda4
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-06 18:12:24 -0500

    comb2

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -5,26 +5,24 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int a = '0';
-int b = '0';
-
-while (a <= '9' && b <= '9')
+int a;
+int b;
+for (a = 0; a < 10; a++)
 {
-putchar(a);
-putchar(b);
-b++;
-if (b > '9')
+for (b = 0; b < 10; b++)
 {
-a++;
-b = '0';
-}
-if (b != '0' || a != '0' && a < '9' && b < '9')
+putchar(a + '0');
+putchar(b + '0');
+if (a != 9 || b != 9)
 {
 putchar(',');
 putchar(' ');
 }
+}
+b = 0;
 }
 putchar('\n');
 return (0);
