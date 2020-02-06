668380b4e15d4136e675d44017d335f90601900f
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-06 16:51:03 -0500

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numero = '0';
+while (numero <= '9')
+{
+putchar(numero);
+numero++;
+if (numero != '0' && numero <= '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
