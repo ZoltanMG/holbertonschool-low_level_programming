a3a70bc0a3bea64563ef7151e53c117ec1de8549
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-06 15:43:16 -0500

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letra = 'a';
+while (letra == 'z')
+{
+if (letra != 'q' || letra != 'e')
+{
+putchar(letra);
+letra++;
+}
+}
+putchar('\n');
+return (0);
+}
