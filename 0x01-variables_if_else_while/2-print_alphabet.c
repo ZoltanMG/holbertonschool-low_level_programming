e16e164c278e7a786f1a748235fbcaa4a04bde5f
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-06 15:14:57 -0500

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int letra = 97;
+while (letra < 124)
+{
+putchar(letra);
+letra = letra + 1;
+if x(letra == 123)
+{
+putchar('\n');
+}
+}
+return (0);
+}
