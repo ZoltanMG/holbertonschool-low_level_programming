0b3952a8dee977903e2c00ad0e044f67a17f6145
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 15:26:14 +0000

    _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,8 +7,15 @@
  */
 int main(void)
 {
-	char a[9] = 'Holberton';
-
-	_putchar (a);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
