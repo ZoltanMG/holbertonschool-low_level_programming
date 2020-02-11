8ab42ec2163deaf75729ee1ce637af22bb76f42f
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 22:16:36 +0000

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-	for (a = 0; a <= 2; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
@@ -17,15 +17,16 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					if (a == 2 && b == 3)
+					if (a == 2 && b == 4)
 					{
+						return;
+					}
 						_putchar(a + '0');
 						_putchar(b + '0');
 						_putchar(':');
 						_putchar(c + '0');
 						_putchar(d + '0');
 						_putchar('\n');
-					}
 				}
 			}
 		}
