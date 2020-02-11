ce4131086b679f2761861c8748086c21e0d59901
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 18:58:47 +0000

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- * print_sing - sings of numbers.
+ * print_sign - sings of numbers.
  * @n: the int to return.
  * Return: always 0.
  */
-int print_sing(int n)
+int print_sign(int n)
 {
 	if (n > 0)
 	{
 		n = 1;
 		_putchar('+');
 	}
-	if else(n == 0)
+	else if (n == 0)
 	{
 		n = 0;
 		_putchar('0');
