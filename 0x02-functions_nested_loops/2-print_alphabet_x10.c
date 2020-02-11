19a441c571e9f625359d25dd2ff08276262e3d50
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 16:56:24 +0000

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - writes the character c to stdout
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int b;
+
+	for (b = 0; b <= 10; ++b)
+	{
+		for (a = 'a'; a <= 'z'; ++a)
+			_putchar(a);
+		_putchar('\n');
+	}
+}
