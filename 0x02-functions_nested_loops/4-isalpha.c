c025ca7e172620375499c759c223764afe7ecd24
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 18:00:56 +0000

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - check lowercaser
+ * @c: the int to return
+ * Return: 1 if c is lowercase
+ */
+int _isalpha(int c)
+{
+
+	if (c > 96 && c < 123 || c > 65 && c < 91)
+		c = 1;
+	else
+		c = 0;
+	return (c);
+}
