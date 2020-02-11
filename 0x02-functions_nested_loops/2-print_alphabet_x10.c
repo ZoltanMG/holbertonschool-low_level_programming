e7a18e2b3526ecb5abc01cb845e4377865eda4e3
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 17:05:01 +0000

    alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 	char a;
 	int b;
 
-	for (b = 0; b <= 10; ++b)
+	for (b = 0; b <= 9; b++)
 	{
-		for (a = 'a'; a <= 'z'; ++a)
+		for (a = 'a'; a <= 'z'; a++)
 			_putchar(a);
 		_putchar('\n');
 	}
