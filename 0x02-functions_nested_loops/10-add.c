f8e6a9e149e8fe3b6f09b0f873a008426624b008
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 22:07:21 -0500

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 
 /**
- * add - add two numbers
+ *add - add two numbers
  *@a: int 1
  *@b: int 2
- * Retrun: Always 0
+ * Return: Always 0
  */
 int add(int a, int b)
 {
 	int s = a + b;
+
 	return (s);
 }
