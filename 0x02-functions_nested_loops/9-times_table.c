c6179d9407dc08276d6249f1e6ee9f585827d2d2
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 21:48:26 -0500

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *times_table -prints time table
+ *Return: always 0
+ */
+void times_table(void)
+{
+	int a, b, r;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			r = a * b;
+			if ((r / 10) > 0)
+			{
+				_putchar((r / 10) + '0');
+			}
+			else if (b != 0)
+			{
+				_putchar(' ');
+			}
+			_putchar((r % 10) + '0');
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
