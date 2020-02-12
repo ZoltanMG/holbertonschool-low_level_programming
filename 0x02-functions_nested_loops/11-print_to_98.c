f4b348883f13bd41d3b64ec38ef05010a1bf1a84
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-11 22:24:38 -0500

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * print_to_98 - numbers before and after
+ * @n : int
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+
+{
+	if (n <= 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			printf("%i", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n >= 98)
+	{
+		for (n = n; n >= 98; n--)
+		{
+			printf("%i", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
