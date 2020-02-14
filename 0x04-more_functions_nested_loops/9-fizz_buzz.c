b69de4dc26d783f33ca89aecdd2926a1e6441960
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-13 22:41:18 -0500

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - writes the character c to stdout
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 15) == 0)
+			printf("FizzBuzz ");
+		else if ((a % 3) == 0)
+			printf("Fizz ");
+		else if ((a % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", a);
+	}
+	printf("\n");
+
+	return (0);
+}
