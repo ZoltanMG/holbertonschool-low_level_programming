32e348503f42f6a936f88dff9194724091d3c6f5
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-13 23:45:01 -0500

    int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,27 @@
 #include "holberton.h"
-
 /**
- * print_number - int
- * @n: int n
+ * print_number - print number
+ * Return: none
+ * @n: in variable
  */
 void print_number(int n)
 {
-	if (n > 9 && n < 100)
+	int a, b, c, d;
+
+	if (n < 0)
 	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		n = n * -1;
+		_putchar(45);
 	}
-	else if (n >= 0 && n < 10)
-	{
-		_putchar(n + '0');
-	}
-	else if (n < 0)
-	{
-		n = (n * -1);
-		_putchar('-');
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n >= 100 && n < 1000)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar((n % 100) + '0');
-	}
-	else
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n % 1000) / 100) + '0');
-		_putchar(((n % 1000) / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+	d = (n % 10);
+	c = (((n % 100) - d) / 10);
+	b = (((n % 1000) - c - d) / 100);
+	a = ((n - b - c - d) / 1000);
+	if (n > 999 || n < -999)
+		_putchar(a + 48);
+	if (n > 99 || n < -99)
+		_putchar(b + 48);
+	if (n > 9 || n < -9)
+		_putchar(c + 48);
+	_putchar(d + 48);
 }
