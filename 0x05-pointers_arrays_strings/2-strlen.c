ef82fe31d1f99d8c7cb00f8fefb534e7919da573
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-17 16:56:37 -0500

    print puts

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _strlen - count of char.
+ * @s: char.
+ * Return: return is x.
  */
 int _strlen(char *s)
 {
 	int x;
 
-	for(x = 0; x < 256; x++)
+	for (x = 0; x < 256; x++)
 	{
-		if(s[x] == '\0')
+		if (s[x] == '\0')
 			break;
 	}
 	return (x);
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _puts - print a string
+ * @str: pointer
+ */
+void _puts(char *str)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x < 256; x++)
+	{
+		if (str[x] == '\0')
+		{
+			z = x;
+			for (y = 0; y <= z; y++)
+			{
+				_putchar(str[y]);
+			}
+			_putchar('\n');
+			break;
+		}
+	}
+}
