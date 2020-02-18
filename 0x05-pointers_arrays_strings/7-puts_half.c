5ab1253689e3012f0665e5148bd797b1add6d5f8
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-18 18:09:12 -0500

    half_print

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * puts_half - half print
+ * @str: pointer
+ */
+void puts_half(char *str)
+{
+	int a;
+	int n;
+	int b;
+
+	for (a = 0; a < 500; a++)
+	{
+		if (str[a] == '\0')
+		{
+			break;
+		}
+	}
+
+	if ((a % 2) == 1)
+	{
+		n = (a - 1) / 2;
+	}
+	else
+		n = a / 2;
+	for (b = n; b < a; b++)
+	{
+		_putchar(str[b]);
+
+	}
+	_putchar('\n');
+}
