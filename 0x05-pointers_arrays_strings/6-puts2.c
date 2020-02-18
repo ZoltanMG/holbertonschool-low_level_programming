d40b40592911008f21734ec5d104c11aeec80a28
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-18 17:19:58 -0500

    puts_2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts2 - puts 2
+ * @str: pointer
+ */
+void puts2(char *str)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 500 ; a++)
+	{
+		if (str[a] == '\0')
+			break;
+	}
+	for (b = 0; b <= a; b++)
+	{
+		_putchar(str[b]);
+		b++;
+	}
+	_putchar('\n');
+}
