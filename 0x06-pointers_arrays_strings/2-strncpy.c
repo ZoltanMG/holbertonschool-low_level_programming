d51cb360d3a6d74eebdeda7a7461cf26680525d8
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-20 18:56:57 +0000

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,16 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * _strncpy - function
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int count;
+
 	for (count = 0; count < n && src[count] != '\0'; count++)
 	{
 		dest[count] = src[count];
