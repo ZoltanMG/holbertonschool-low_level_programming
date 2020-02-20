1d6e52ae448dcf4a2075183f620010e8e07058be
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-20 22:32:57 +0000

    string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * string_toupper
+ * @i: char
+ * Retrun: i
+ */
+char *string_toupper(char *i)
+{
+	int count;
+	int aux;
+
+	for (count = 0; i[count] != '\0'; count++)
+	{
+		if (i[count] > 96 && i[count] < 123)
+		{
+			aux = i[count];
+			aux = aux - 32;
+			i[count] = aux;
+		}
+	}
+	return (i);
+}
