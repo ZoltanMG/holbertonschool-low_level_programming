1abaf4f0f7773dc8d44a7d3608cfe30e6c8d5604
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-21 01:34:19 +0000

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,23 +7,21 @@
  */
 char *cap_string(char *str)
 {
-	int count;
+	int x;
 	int count2;
-	int aux;
-
-	char crt[12] = {' ', '\t', '\n', ',', ';', '!', '?', '"', '(', ')',
+	char crt[13] = {' ', '\t', '\n', ',', '.', ';', '!', '?', '"', '(', ')',
 			'{', '}'};
 
-	for (count = 0; str[count] != '\0'; count++)
+	for (x = 0; str[x] != '\0'; x++)
 	{
-		for (count2 = 0; crt[count2] != '\0'; count2++)
+		if (str[0] < 123 && str[0] > 96)
+			str[x] -= 32;
+		for (count2 = 0; count2 < 13; count2++)
 		{
-			if (str[count] == crt[count2] && (str[count + 1] < 65 &&
-							  str[count + 1] > 90))
+			if (str[x] == crt[count2] && (str[x + 1] < 123
+						      && str[x + 1] > 96))
 			{
-				aux = str[count + 1];
-				aux = aux - 32;
-				str[count + 1] = aux;
+				str[x + 1] -= 32;
 			}
 		}
 	}
