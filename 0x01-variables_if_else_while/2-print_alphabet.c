f767c2dd754713ababf18cd5080119a56fed2f17
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-21 22:07:55 -0500

    my first while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - while loop
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int letra = 'a';
-while (letra <= 'z')
-{
-putchar(letra);
-letra++;
-}
-putchar('\n');
-return (0);
+	int letra = 'a';
+	while (letra <= 'z')
+	{
+		putchar(letra);
+		letra++;
+	}
+	putchar('\n');
+	return (0);
 }
