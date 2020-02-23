5098d41d7c4b7253af2501d7297eb2b24e5d9ba0
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-23 14:12:23 -0500

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-int numero = '0';
-while (numero <= '9')
-{
-putchar(numero);
-numero++;
-if (numero != '0' && numero <= '9')
-{
-putchar(',');
-putchar(' ');
-}
-}
-putchar('\n');
-return (0);
+	int numero = '0';
+	while (numero <= '9')
+	{
+		putchar(numero);
+		numero++;
+		if (numero != '0' && numero <= '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
