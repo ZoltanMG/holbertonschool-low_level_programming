7c23702228968efeba4ba4f94abf44c57626b254
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-24 17:51:48 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - the function
+ * @s: pointer
+ * @c: char
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	int count;
+	int b = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		if (s[count] == c)
+		{
+			s++;
+			return (s);
+		}
+		s++;
+	}
+	return(NULL);
+}
