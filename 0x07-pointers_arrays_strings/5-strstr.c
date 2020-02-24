00b66082f14e46e6b7cfadfafd036741add93d80
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-02-24 22:38:08 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,16 +10,24 @@ char *_strstr(char *haystack, char *needle)
 {
 	int a;
 	int c;
+	int i = 0;
+	int b;
+	int r = 0;
+
+	for (b = 0; haystack[b] != '\0'; b++)
+	{}
 
 	for (a = 0; needle[a] != '\0'; a++)
 	{
 		for (c = 0; haystack[c] != '\0'; c++)
 		{
 			if (needle[a] == haystack[c])
-				haystack++;
+			{
+				i++;
+				r = b - i;
+				break;
+			}
 		}
 	}
-	if (needle[a] == haystack[c])
-		haystack++;
-	return (haystack);
+	return (haystack + r);
 }
