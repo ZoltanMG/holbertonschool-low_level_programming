30a4766c8ddcf7efcac79c8ad51c5fab1477c463
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-01 11:40:31 -0500

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - function
+ * @argc: int
+ * @argv: char
+ * Return: o
+ */
+int main(int argc, char *argv[])
+{
+	int i = argc - 1;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
