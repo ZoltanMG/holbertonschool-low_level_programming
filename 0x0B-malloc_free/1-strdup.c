d4947a3d22f828f8a580c370964561f77bc593cb
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-03 19:28:36 +0000

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,17 +11,20 @@ char *_strdup(char *str)
 	char *ptr;
 	int c, i;
 
-	if (*str == '\0')
+	if (str == NULL)
 		return (NULL);
 	for (c = 0; str[c] != '\0'; c++)
 	{
 	}
+
 	ptr = malloc(c + 1 * sizeof(char));
 
-	for (i = 0; str[i] != '\0'; i++)
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < c; i++)
 	{
 		ptr[i] = str[i];
 	}
-	ptr[i] = '\0';
 	return (ptr);
 }
