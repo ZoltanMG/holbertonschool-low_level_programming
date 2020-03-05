e51a1139fb26cabf741ff48b0cd53345994fe37e
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-04 22:43:36 -0500

    argstostr

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,34 +1,45 @@
+#include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * argstostr - function
+ * @ac: int
+ * @av: char
+ * Return: arr
+ */
 char *argstostr(int ac, char **av)
 {
-	char *arr = NULL;
+	char *arr;
 	int cont;
 	int cont2;
-	int cont3;
+	int cont3 = 0;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
+
 	for (cont = 0; cont < ac; cont++)
 	{
-		for (cont2 = 0; av[cont][cont2] != '\0'; cont2++)
+		for (cont2 = 0; av[cont][cont2]; cont2++)
 		{
 			cont3++;
 		}
+		cont3++;
 	}
-	arr = malloc((cont3 + ac + 1) * sizeof(char));
+	cont3++;
+
+	arr = malloc((cont3 + ac) * sizeof(char));
+
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
+
 	cont3 = 0;
+
 	for (cont = 0; cont < ac; cont++)
 	{
-		for (cont2 = 0; av[cont][cont2] != '\0'; cont2++)
+		for (cont2 = 0; av[cont][cont2]; cont2++)
 		{
 			arr[cont3] = av[cont][cont2];
+			cont3++;
 		}
 		arr[cont3] = '\n';
 		cont3++;
