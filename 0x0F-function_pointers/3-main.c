4e70290df8ded349fa0b010f6065b077e00cc7ce
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-11 23:54:22 +0000

    main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function
+ * @argc: int
+ * @argv: char
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int res;
+
+	if (argc < 1 && argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' &&
+	    argv[2][0] != '*' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+
+	}
+
+	if ((argv[2][0] == '/' && argv[3][0] == '0') ||
+	    (argv[2][0] == '%' && argv[3][0] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	res = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("el resultado es: %d\n", res);
+	return (0);
+}
