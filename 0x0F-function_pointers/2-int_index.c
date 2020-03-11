d4764f7ba528e2eef1111e8bfe7fc059ce8b6ae2
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-11 20:47:55 +0000

    change structur of if

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,14 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (array || cmp)
+	if (!array || !cmp)
+		return (-1);
+	for (a = 0; a < size; a++)
 	{
-		for (a = 0; a < size; a++)
-		{
-			b = (*cmp)(array[a]);
-			if (b == 1)
-				return (a);
-		}
+		b = (*cmp)(array[a]);
+		if (b == 1)
+			return (a);
 	}
 	return (-1);
 }
