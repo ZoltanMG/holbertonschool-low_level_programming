69cc4a171ea62dbfa645e977f3026c2817977e83
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-11 20:45:06 +0000

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int a;
 	int b;
 
+	if (size <= 0)
+		return (-1);
 	if (array || cmp)
 	{
-		if (size <= 0)
-			return (-1);
 		for (a = 0; a < size; a++)
 		{
 			b = (*cmp)(array[a]);
-			if (b != 0)
+			if (b == 1)
 				return (a);
 		}
 	}
