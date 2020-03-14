0c4509cc66c29a0cdbf9b1d91453bf0e9a69f4d9
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-14 01:33:50 +0000

    print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 
+#include <stdarg.h>
+
+/**
+ * struct form_t - struct
+ * @fmt: char
+ * @f: pointer to function
+ */
+typedef struct form_t
+{
+	char *fmt;
+	void (*f)(va_list);
+} form;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
