75223f4f3a3b936aae853877c9fc47130747e7e7
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-26 20:20:16 -0500

    [Add]: function that print numbers base 10 from base 16

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - function
+ * @b: string whit '0' or '1'
+ *
+ * Return: number in base 10
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a = 0;
+	int pot = 1;
+	int num_d = 0;
+
+	while (b[a])
+	{
+		if ((b[a] != '1' && b[a] != '0') || (b[a] == '\0'))
+			return (0);
+		a++;
+	}
+
+	if (a !=  1)
+		a--;
+	else
+		return (1);
+	while (a >= 0)
+	{
+		if (b[a] != '0')
+			num_d += pot;
+		a--;
+		pot += pot;
+	}
+	return (num_d);
+}
