041759656057dd00875123835fccdfa6b51e2a4d
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-27 20:08:00 -0500

    [add]: refacture

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,40 +6,16 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int pot_1 = 1, pot_2, dup = n;
-	int count_1 = 0, count_2 = 1;
-
 	if (n == 0)
-		_putchar(n + '0');
-	else
 	{
-		while (pot_1 <= dup)
-		{
-			if (pot_1 <= dup)
-				pot_2 = pot_1;
-			pot_1 += pot_1;
-			count_1++;
-		}
-		dup -= pot_2;
-		_putchar('1');
-		while (pot_2 > 1)
-		{
-			pot_1 = 1;
-			count_1--;
-			count_2 = 1;
-			while (count_2 < count_1)
-			{
-				pot_1 += pot_1;
-				count_2++;
-			}
-			if (pot_1 <= dup)
-			{
-				_putchar('1');
-				dup -= pot_1;
-			}
-			else
-				_putchar('0');
-			pot_2 = pot_1;
-		}
+		putchar('0');
+		return;
 	}
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+		putchar((n & 1) + '0');
+	}
+	else
+		putchar('1');
 }
