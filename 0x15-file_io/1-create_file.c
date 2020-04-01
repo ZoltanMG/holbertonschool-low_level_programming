6dbaa3d67c37c7a58dc9928282be84be79945490
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-31 19:26:45 -0500

    [update]: add more close

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,19 +11,25 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, count = 0, rw;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (fd);
+	if (!text_content)
+	{
+		close(fd);
+		return (-1);
+	}
 	while (text_content[count])
 		count++;
-
 	rw = write(fd, text_content, count);
 	if (rw == -1)
-		close (fd);
+	{
+		close(fd);
 		return (rw);
+	}
 	close(fd);
 
 	return (1);
