8dc7c1cb80e29dfcb7fc836a002a5bfd9798d973
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-31 19:11:17 -0500

    [update]: add close

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,10 @@ int create_file(const char *filename, char *text_content)
 		return (fd);
 
 	if (!text_content)
+	{
+		close (fd);
 		return (-1);
+	}
 	while (text_content[count])
 	{
 		count++;
