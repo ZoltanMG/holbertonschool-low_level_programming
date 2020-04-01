282db7932ef0dafd6844f127d230be42725bd08f
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-31 19:30:59 -0500

    [update]: add -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
-		return (fd);
+		return (-1);
 	if (!text_content)
 	{
 		close(fd);
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (rw == -1)
 	{
 		close(fd);
-		return (rw);
+		return (-1);
 	}
 	close(fd);
 
