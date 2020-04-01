243f43ccfcd7c9b7f6e4e2e67aefe4997a0fad31
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-03-31 19:13:30 -0500

    [update]: refacture

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,14 +18,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (fd);
 
+	while (text_content[count])
+	{
+		count++;
+	}
 	if (text_content = NULL)
 	{
 		close (fd);
 		return (-1);
-	}
-	while (text_content[count])
-	{
-		count++;
 	}
 	rw = write(fd, text_content, count);
 	if (rw == -1)
