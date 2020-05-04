cc9f1e2d09a00a0b14a8e3b1ed7468dc09b9df97
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-03 22:12:12 -0500

    [update]: add 1 mora edge case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,18 +13,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *aux, *new;
 	unsigned int count = 0;
 
+	if (!h)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-	if (*h == NULL && idx == 0)
+	if (*h == NULL)
 	{
 		*h = new;
 		return (new);
 	}
-
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	aux = *h;
 	while (aux)
 	{
@@ -41,6 +44,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		aux = aux->next;
 		count++;
 	}
-	free(new);
 	return (NULL);
 }
