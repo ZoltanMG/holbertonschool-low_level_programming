7ae6e1d605eaeff9e1926b91dfa5b214a68aad47
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-30 21:04:54 -0500

    [update] add pointer

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	hash_node_t node;
+	hash_node_t *node;
 
 	table = malloc(sizeof(hash_table_t *));
 	if (!table)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof((node)) * size);
+	table->array = malloc(sizeof(node) * size);
 	if (table->array == NULL)
 		return (NULL);
 	return (table);
