68c13ea7bf415b9e72ffe7c417b844fa87b906e7
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-30 21:02:33 -0500

    [update]: refacture

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,16 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	hash_node_t node;
 
-	table = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t *));
 	if (!table)
 		return (NULL);
 
 	table->size = size;
 	table->array = malloc(sizeof((node)) * size);
-	if (!(table->array))
-	{
-		free(table);
+	if (table->array == NULL)
 		return (NULL);
-	}
 	return (table);
 }
