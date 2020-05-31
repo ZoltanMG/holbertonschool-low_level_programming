90de4c9f6673e8c628496288145732a84913280d
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-30 21:08:46 -0500

    [update]: refacture

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,9 @@
-#ifndef HASH_TABLES_H
-#define HASH_TABLES_H
+#ifndef TABLE_H
+#define TABLE_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -41,5 +41,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
 
 #endif
