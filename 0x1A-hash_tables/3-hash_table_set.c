738bb6fc9e332aff2f3e51ebbcfecfd70d4fd018
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-05-31 18:06:38 -0500

    [update]: valide Betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (aux)
 	{
 		while (aux->next != NULL)
-        		aux = aux->next;
+			aux = aux->next;
 		aux->next = node;
 	}
 	else
