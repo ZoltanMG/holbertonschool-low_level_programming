c0f94c2470ee1d8001df52c738732b7147aa6d74
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-06-18 22:34:07 +0000

    [update]: change print option

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,13 +9,14 @@
 size_t print_list(const list_t *h)
 {
 	size_t n_nodo;
+	char *nil = "(nil)";
 
 	if (h == NULL)
 		return (0);
 	while (h)
 	{
 		if (!(h->str))
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, nil);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
