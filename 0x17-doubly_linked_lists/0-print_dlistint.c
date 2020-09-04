907f174c400cafa4b9d663bd9e5c388e38e8fe18
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-04 15:36:34 +0000

    [update]: valide Betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints elements of a dlistint_t list.
+ * @h: linked list.
+ *
+ * Return: list long.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
