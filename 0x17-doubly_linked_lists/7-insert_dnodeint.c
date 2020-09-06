b12e459c9b5ef3ac6401f9cf9f97fe727151a0ef
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-06 01:09:01 +0000

    [update]: refacture

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,67 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *head = NULL;
+	unsigned int count = 0;
+	dlistint_t *head = *h;
 	dlistint_t *new = NULL;
+
+	while (head)
+	{
+		count++;
+		head = head->next;
+	}
+
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	else if (idx > 0)
+	{
+		new = add_node_num(h, idx, n);
+		return (new);
+	}
+	else if (idx == count)
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
+	}
+	return (NULL);
+}
+
+/**
+ * add_node_num - add node in linked list
+ * @h: linked list.
+ * @idx: number of index
+ * @n: element to linked list
+ *
+ * Return: new nodw
+ */
+dlistint_t *add_node_num(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = NULL;
+	dlistint_t *head = *h;
 	unsigned int count = 1;
 
-	head = *h;
-
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+	if (!new)
+		return (new);
+
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-
-	if (*h == NULL)
-		*h = new;
-	else if (idx == 0)
+	while (head)
 	{
-		new->next = head;
-		head->next->prev = new;
-		*h = new;
-		return (new);
-	}
-	else
-	{
-		while (head)
+		if (idx == count)
 		{
-			if (count == idx)
-			{
-				new->prev = head;
-				new->next = head->next;
-				head->next->prev = new;
-				head->next = new;
-				return (new);
-			}
-			head = head->next;
-			count++;
+			new->next = head->next;
+			head->next->prev = new;
+			new->prev = head;
+			head->next = new;
+			return (new);
 		}
+		head = head->next;
+		count++;
 	}
 	return (NULL);
 }
