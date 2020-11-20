3e6bb543a1ac89d5f0540e0a9378cd0dc7e19711
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-11-20 23:33:43 +0000

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
+
 """
-documentation
+matriz
 """
 
 
 def island_perimeter(grid):
     """
-    class
+    retornar el perimetro de la isla
     """
-
-    pass
+    perimeter = 0
+    for columna in range(len(grid)):
+        for fila in range(len(grid[columna])):
+            if grid[columna][fila] == 1:
+                if columna - 1 < 0 or grid[columna - 1][fila] == 0:
+                    perimeter += 1
+                # aqui lo valida con el len de la matriz ya que este
+                # es elmismo numero de cols
+                if columna + 1 >= len(grid) or grid[columna + 1][fila] == 0:
+                    perimeter += 1
+                if fila + 1 >= len(grid[0]) or grid[columna][fila + 1] == 0:
+                    perimeter += 1
+                if fila - 1 < 0 or grid[columna][fila - 1] == 0:
+                    perimeter += 1
+    return perimeter
