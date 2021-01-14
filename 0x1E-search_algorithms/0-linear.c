d67ed358809c5fc1ff96ef5102909f583c7527d2
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-01-14 21:57:24 +0000

    [updated]: add edge case

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,8 +13,8 @@ int linear_search(int *array, size_t size, int value)
 	int t_size = size;
 	int count;
 
-	if (array == NULL)
-		return(-1);
+	if (array == NULL || !size)
+		return (-1);
 	for (count = 0; count <= t_size; count++)
 	{
 		if (array[count] == value)
